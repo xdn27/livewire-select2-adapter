adf8bfcd31632e0a0185b8d4c2027e8e3f7a73ce
Denisa Rachman <27.idea@gmail.com>
2025-07-24 10:44:30 +0700

    show option if there is existing data

--- src/LivewireSelect2Adapter.js
+++ src/LivewireSelect2Adapter.js
@@ -1,14 +1,3 @@
-/**
- * Livewire Select2 Adapter (xdn27)
- * Version: 1.2.2
- * Enables seamless integration between Select2 and Laravel Livewire.
- *
- * @author
- * xdn27 - https://github.com/xdn27
- *
- * @license MIT
- */
-
 (function (factory) {
   if (typeof define === "function" && define.amd) {
     define(["jquery", "select2"], factory);
@@ -28,6 +17,21 @@
   var ArrayAdapter = $.fn.select2.amd.require("select2/data/array");
 
   function LivewireModelAdapter($element, options) {
+    // Simpan data existing sebelum memanggil ArrayAdapter
+    var self = this;
+    this._existingOptions = [];
+    $element.find("option").each(function () {
+      var $option = $(this);
+      self._existingOptions.push({
+        id: $option.val(),
+        text: $option.text().trim(),
+        selected: $option.prop("selected"),
+        disabled: $option.prop("disabled"),
+        element: this,
+      });
+    });
+
+    // Panggil konstruktor ArrayAdapter
     ArrayAdapter.call(this, $element, options);
 
     this._defaultEventName = "select2-results";
@@ -39,13 +43,16 @@
         emitName: this._defaultEmitName,
         eventOnSelect: "model",
         delay: 300,
+        version: 3,
       },
       options.get("livewire") || {}
     );
 
     this._element = $element;
     this._wireModel =
-      $element.attr("wire:model") || $element.attr("wire:model.live");
+      $element.attr("wire:model") ||
+      $element.attr("wire:model.live") ||
+      $element.attr("wire:model.defer");
 
     this._registeredEventName = this.livewireConfig.eventName;
     if (
@@ -80,8 +87,12 @@
 
     this._eventHandler = function (e) {
       if (self._pendingCallback) {
+        // Gabungkan data existing dengan data baru dari Livewire
+        var combinedResults = self._existingOptions.concat(
+          e.detail.results || []
+        );
         self._pendingCallback({
-          results: e.detail[0].results,
+          results: combinedResults,
         });
         self._pendingCallback = null;
       }
@@ -90,7 +101,13 @@
     window.addEventListener(this._registeredEventName, this._eventHandler);
 
     if (self._element.prop("multiple")) {
-      // Not supported yet
+      //  We dont support yet
+      // this._element.on("select2:select", function (e) {
+      //   self._handleSelection(e);
+      // });
+      // this._element.on("select2:unselect", function (e) {
+      //   self._handleSelection(e);
+      // });
     } else {
       this._element.on("select2:select", function (e) {
         self._handleSelection(e);
@@ -124,7 +141,25 @@
           if (component) {
             var isDeferred = !model.includes(".live");
             var value = e.params.data.id;
-            component.set(model, value, isDeferred);
+
+            if (typeof component.set === "function") {
+              // Livewire 3
+              component.set(model, value, isDeferred);
+            } else if (typeof component.__instance !== "undefined") {
+              // Livewire 2
+              if (isDeferred) {
+                component.__instance.call("deferSet", model, value);
+              } else {
+                component.__instance.set(model, value, true);
+              }
+            } else if (typeof component.$wire !== "undefined") {
+              // Alternatif untuk Livewire 2
+              if (isDeferred) {
+                component.$wire.deferSet(model, value);
+              } else {
+                component.$wire.set(model, value, true);
+              }
+            }
           }
         }
       }
@@ -152,9 +187,24 @@
   LivewireModelAdapter.prototype._attachLivewireReinit = function () {
     if (!window.__livewire_select2_reinit_hook_attached) {
       window.__livewire_select2_reinit_hook_attached = true;
+
       document.addEventListener("livewire:init", function () {
         if (window.Livewire && window.Livewire.hook) {
           window.Livewire.hook("morphed", function () {
+            $("[data-select2-livewire]").each(function () {
+              var $el = $(this);
+              if ($el.data("select2")) {
+                var config = $el.data("select2").options.options;
+                $el.select2("destroy").select2(config);
+              }
+            });
+          });
+        }
+      });
+
+      window.addEventListener("livewire:load", function () {
+        if (window.Livewire && window.Livewire.hook) {
+          window.Livewire.hook("message.processed", function () {
             $("[data-select2-livewire]").each(function () {
               var $el = $(this);
               if ($el.data("select2")) {
@@ -172,24 +222,44 @@
     var self = this;
     clearTimeout(this._queryTimeout);
 
+    // Jika pencarian kosong, tampilkan data existing
+    if (!params.term || params.term === "") {
+      callback({ results: this._existingOptions });
+      return;
+    }
+
     this._queryTimeout = setTimeout(function () {
-      self._pendingCallback = callback;
+      self._pendingCallback = function (response) {
+        callback(response);
+        // Setelah menerima hasil, buka dropdown jika tertutup
+        setTimeout(function () {
+          if (!self._element.data("select2").isOpen()) {
+            self._element.select2("open");
+          }
+        }, 50);
+      };
+
       if (window.Livewire) {
         var emitName = self.livewireConfig.emitName;
         if (emitName === self._defaultEmitName && self._wireModel) {
           emitName += "-" + self._wireModel;
         }
+
+        // Simpan state dropdown sebelum emit
+        var isDropdownOpen = self._element.data("select2").isOpen();
+
         if (typeof window.Livewire.dispatch === "function") {
           window.Livewire.dispatch(emitName, params);
         } else if (typeof window.Livewire.emit === "function") {
           window.Livewire.emit(emitName, params);
-        } else {
-          console.error(
-            "[LivewireSelect2Adapter] Livewire emit/dispatch is not available."
-          );
         }
-      } else {
-        console.error("[LivewireSelect2Adapter] Livewire is not available.");
+
+        // Jika dropdown tertutup, buka kembali setelah emit
+        if (!isDropdownOpen) {
+          setTimeout(function () {
+            self._element.select2("open");
+          }, 100);
+        }
       }
     }, this.livewireConfig.delay);
   };
@@ -205,18 +275,11 @@
     }
     if (this._element) {
       this._element.off("select2:select");
+      this._element.off("select2:unselect");
     }
-  };
-
-  LivewireModelAdapter.prototype.render = function () {
-    return ArrayAdapter.prototype.render.call(this);
-  };
-
-  LivewireModelAdapter.prototype.bind = function (container, $container) {
-    return ArrayAdapter.prototype.bind.call(this, container, $container);
   };
 
   window.select2 = window.select2 || {};
   window.select2.LivewireModelAdapter = LivewireModelAdapter;
-  window.select2.LivewireModelAdapterVersion = "1.2.0";
+  window.select2.LivewireModelAdapterVersion = "1.2.4";
 });
